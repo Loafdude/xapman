7cf7519e670f1fb125f0840ee5eb8bbe05da3c18
Dean Rispin <drispin@marinepartssupply.com>
2018-03-21 14:29:23 -0700

    Fix :'s

--- xapman.py
+++ xapman.py
@@ -104,7 +104,7 @@ class XapUnit(object):
         self.safety_mute = status
         return status
         
-    def setSafetyMute(self, isEnabled)
+    def setSafetyMute(self, isEnabled):
         '''Set safety mute status to XAP Unit'''
         status = self.connection.setSafetyMute(isEnabled, unitCode=self.device_id)
         self.safety_mute = status
@@ -168,94 +168,94 @@ class OutputChannel(object):
         self.label = label
         return label
         
-class InputChannel(object):
-    """XAP Input Channel Wrapper"""
-
-    def __init__(self, xap_connection, title
-                 mqtt_path="home/HA/AudioMixer/Inputs",
-                 alt_mqtt_paths=[],
-                 XAP_Unit=0,
-                 XAP_Channel=1,
-                 AutoApplyLabel=True):
-
-        self.title          = title
-        self.mqtt_path      = mqtt_path
-        self.alt_mqtt_paths = alt_mqtt_paths
-        self.gain           = None
-        self.gain_min       = None
-        self.gain_max       = None
-        self.mute           = None
-        self.label          = None
-        self.type               = None # LineLevel or Microphone
-        self.AGC                = None # True or False - Automatic Gain Control
-        self.AGC_target         = None # -30 to 20dB
-        self.AGC_threshold      = None # -50 to 0dB
-        self.AGC_attack         = None # 0.1 to 10.0s in .1 increments
-        self.AGC_gain           = None # 0.0 to 18.0dB  
-        
-        # Microphone Input Only
-        self.phantom_power      = None
-        self.NC                 = None # True or False - Noise Cancellation
-        self.NC_depth           = None # 6 to 15dB
-        self.AEC                = None # True or False - Acoutstic Echo Canceller
-        self.AEC_PA_reference   = None # None or OutputChannel
-        self.NLP                = None # False = Off, Soft, Medium, Aggresive - Non-Linear Processing
-        self.filters            = None # Max 4. List of filters?
-        self.bypass_filters     = None # True or False
-        self.gating             = None # False, Manual On, Manual Off
-        self.gate_holdtime      = None # 0.10 - 8.00s
-        self.gate_override      = None # True or False
-        self.gate_ratio         = None # 0-50dB
-        self.gate_group         = None # 1-4 and A-D (gate group)
-        self.gate_chairman      = None # True or False
-        self.gate_attenuation   = None # 0-60dB
-        self.gate_decay         = None # Slow, Medium, Fast
-        self.adaptive_ambient   = None # True or False
-        self.ambient_level      = None # -80.0 to 0.0dB
-        self.PA_adaptive        = None # True or False
-        
-        
-        
-        
-        
-        
-        
-        
-
-class ProcessingChannel(object):
-    self.label          = None
-    self.delay = None
-    self.mute = None
-    self.compressor = None # True or False
-    self.compressor_group = None # 
-    self.compressor_post_gain = None # 
-    self.compressor_threshold = None 
-    self.compressor_ratio = None
-    self.compressor_attack = None
-    self.compressor_release = None
-    self.filters = None
-    self.delay = None
-    self.gain = None
-    
-
-class ExpansionChannel(object):
-    self.input_label          = None
-    self.output_label          = None
-
-class GatingGroup(object):
-    self.max_mics = None # 1 to 64
-    self.first_mic_priority = None # True or False
-    self.last_mic_mode = None # True, False
-    self.last_mic_group = None # 1-8
-
-    
-
-class Filter(object):
-    self.type = None # All Pass, High Pass, Low Pass, Notch, PEQ
-    self.frequency = None
-    self.gain = None
-    self.Q = None
-    self.bandwidth = None
-    self.bypass = None
-    self.phase = None
-    
+# class InputChannel(object):
+#     """XAP Input Channel Wrapper"""
+#
+#     def __init__(self, xap_connection, title
+#                  mqtt_path="home/HA/AudioMixer/Inputs",
+#                  alt_mqtt_paths=[],
+#                  XAP_Unit=0,
+#                  XAP_Channel=1,
+#                  AutoApplyLabel=True):
+#
+#         self.title          = title
+#         self.mqtt_path      = mqtt_path
+#         self.alt_mqtt_paths = alt_mqtt_paths
+#         self.gain           = None
+#         self.gain_min       = None
+#         self.gain_max       = None
+#         self.mute           = None
+#         self.label          = None
+#         self.type               = None # LineLevel or Microphone
+#         self.AGC                = None # True or False - Automatic Gain Control
+#         self.AGC_target         = None # -30 to 20dB
+#         self.AGC_threshold      = None # -50 to 0dB
+#         self.AGC_attack         = None # 0.1 to 10.0s in .1 increments
+#         self.AGC_gain           = None # 0.0 to 18.0dB
+#
+#         # Microphone Input Only
+#         self.phantom_power      = None
+#         self.NC                 = None # True or False - Noise Cancellation
+#         self.NC_depth           = None # 6 to 15dB
+#         self.AEC                = None # True or False - Acoutstic Echo Canceller
+#         self.AEC_PA_reference   = None # None or OutputChannel
+#         self.NLP                = None # False = Off, Soft, Medium, Aggresive - Non-Linear Processing
+#         self.filters            = None # Max 4. List of filters?
+#         self.bypass_filters     = None # True or False
+#         self.gating             = None # False, Manual On, Manual Off
+#         self.gate_holdtime      = None # 0.10 - 8.00s
+#         self.gate_override      = None # True or False
+#         self.gate_ratio         = None # 0-50dB
+#         self.gate_group         = None # 1-4 and A-D (gate group)
+#         self.gate_chairman      = None # True or False
+#         self.gate_attenuation   = None # 0-60dB
+#         self.gate_decay         = None # Slow, Medium, Fast
+#         self.adaptive_ambient   = None # True or False
+#         self.ambient_level      = None # -80.0 to 0.0dB
+#         self.PA_adaptive        = None # True or False
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# class ProcessingChannel(object):
+#     self.label          = None
+#     self.delay = None
+#     self.mute = None
+#     self.compressor = None # True or False
+#     self.compressor_group = None #
+#     self.compressor_post_gain = None #
+#     self.compressor_threshold = None
+#     self.compressor_ratio = None
+#     self.compressor_attack = None
+#     self.compressor_release = None
+#     self.filters = None
+#     self.delay = None
+#     self.gain = None
+#
+#
+# class ExpansionChannel(object):
+#     self.input_label          = None
+#     self.output_label          = None
+#
+# class GatingGroup(object):
+#     self.max_mics = None # 1 to 64
+#     self.first_mic_priority = None # True or False
+#     self.last_mic_mode = None # True, False
+#     self.last_mic_group = None # 1-8
+#
+#
+#
+# class Filter(object):
+#     self.type = None # All Pass, High Pass, Low Pass, Notch, PEQ
+#     self.frequency = None
+#     self.gain = None
+#     self.Q = None
+#     self.bandwidth = None
+#     self.bypass = None
+#     self.phase = None
+#
