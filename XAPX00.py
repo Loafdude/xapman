94e9ac803518cab564dd66b1af5362644ca684e4
Dean Rispin <drispin@marinepartssupply.com>
2018-03-22 09:48:39 -0700

    Add xap400 matrix channels

--- XAPX00.py
+++ XAPX00.py
@@ -56,7 +56,7 @@ XAP800_UNIT_TYPE = 1
 EOM = "\r"
 DEVICE_MAXMICS = "Max Number of Microphones"
 matrixGeo = {'XAP800': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, "0", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H"],
-             'XAP400': 8}
+             'XAP400': [1, 2, 3, 4, 5, 6, 7, 8, "0", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D"]}
 nogainGroups = ('E')
 
 def stereo(func):
