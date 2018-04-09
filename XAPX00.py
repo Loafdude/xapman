0a425b8391424486113f7e21e37f881c9e762e50
User <rispin@gmail.com>
2018-04-08 19:29:22 -0700

    fixing serial decoder

--- XAPX00.py
+++ XAPX00.py
@@ -453,7 +453,7 @@ class XAPX00(object):
             if self.write_to_object:
                 setattr(getattr(self.object, self.unit_attribute)[unit], 'panel_passcode', value)
         elif command == "CHAIRO":
-            channel, group, value = convertToInt(res[2]), convertToInt(res[3]), bool(int(res[4]))
+            channel, value = convertToInt(res[2]), bool(int(res[3]))
             if self.write_to_object:
                 setattr(getattr(getattr(self.object, self.unit_attribute)[unit], self.input_attribute)[channel],
                         'gate_chairman', value)
