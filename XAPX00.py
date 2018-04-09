9b7ad104e28438d8edfa16704bf603390e85d4fd
Dean Rispin <drispin@marinepartssupply.com>
2018-04-09 12:29:24 -0700

    refactoring connect()

--- XAPX00.py
+++ XAPX00.py
@@ -244,10 +244,10 @@ class XAPX00(object):
         for serdata in self.serial.readlines(5000):
             resp = str(serdata).strip().split('#')
             print(str(resp))
-            if len(resp) == 2:
+            if len(resp) is 2:
                 data = resp[1].split()
                 print(str(data))
-                if len(data) > 4:
+                if len(data) > 3:
                     type, did, cmd, value1, value2 = data[0][0:1], data[0][1:2], data[1], data[2], data[3]
                     print(str(cmd))
                     if cmd is command:
