e8b2b18c0a78b1447877caf773327e7ecae16cdf
Dean Rispin <drispin@marinepartssupply.com>
2018-04-09 12:31:10 -0700

    refactoring connect()

--- XAPX00.py
+++ XAPX00.py
@@ -249,7 +249,7 @@ class XAPX00(object):
                 print(str(data))
                 if len(data) > 3:
                     type, did, cmd, value1, value2 = data[0][0:1], data[0][1:2], data[1], data[2], data[3]
-                    if cmd is command:
+                    if str(cmd) is str(command):
                         print(str(cmd))
                         result.append([type, did, cmd, value1, value2])
         return result
