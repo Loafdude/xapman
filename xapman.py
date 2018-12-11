2054f0bf06d50d2cdc3fac708fb07892d4733c5e
Dean Rispin <drispin@marinepartssupply.com>
2018-12-10 20:59:47 -0800

    add gating updates

--- xapman.py
+++ xapman.py
@@ -450,23 +450,23 @@ class XapUnit(object):
     def initialize(self):
         if self.connection.initialize is True:
             self.refreshData()
-            # self.scanOutputChannels()
-            # self.scanInputChannels()
-            # self.scanMatrix()
-            # print("  Scanning Output Channels...")
-            # for id, channel in self.output_channels.items():
-            #     channel.initialize()
-            # print("  Scanning Input Channels...")
-            # for id, channel in self.input_channels.items():
-            #     channel.initialize()
+            self.scanOutputChannels()
+            self.scanInputChannels()
+            self.scanMatrix()
+            print("  Scanning Output Channels...")
+            for id, channel in self.output_channels.items():
+                channel.initialize()
+            print("  Scanning Input Channels...")
+            for id, channel in self.input_channels.items():
+                channel.initialize()
             # print("  Scanning Matrix...")
             # for y, row in self.matrix.items():
             #     for x, matrix_item in row.items():
             #         if matrix_item:
             #             matrix_item.initialize()
-            # print("  Scanning Gating Groups...")
-            # for group, data in self.gating_groups.items():
-            #     self.gating_groups[group].initialize()
+            print("  Scanning Gating Groups...")
+            for group, data in self.gating_groups.items():
+                self.gating_groups[group].initialize()
 
     def refreshData(self):
         """Fetch all data XAP Unit"""
