8b41b705f2b6d385bd79d08382deafc98833afff
Dean Rispin <drispin@marinepartssupply.com>
2018-12-10 19:46:54 -0800

    tidy up

--- mqtt.py
+++ mqtt.py
@@ -1,21 +1,20 @@
 import paho.mqtt.client as mqtt
-import paho.mqtt.publish as publish
 
 class MQTT(mqtt.Client):
 
     def on_connect(self, mqttc, obj, flags, rc):
         for s in self.subscriptions:
             self.subscribe(s)
-        print("rc: "+str(rc))
+        #print("rc: "+str(rc))
 
     def on_publish(self, mqttc, obj, mid):
-        print("mid: " + str(mid))
+        #print("mid: " + str(mid))
 
     def on_subscribe(self, mqttc, obj, mid, granted_qos):
-        print("Subscribed: " + str(mid) + " " + str(granted_qos))
+        #print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
     def on_log(self, mqttc, obj, level, string):
-        print(string)
+        #print(string)
 
     def conn(self):
         self.connect("10.9.8.184", 1883, 60)
