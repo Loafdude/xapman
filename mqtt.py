1b89f0980bc4073511c1f1fa24a010ff6a49db8e
Dean Rispin <drispin@marinepartssupply.com>
2018-12-10 19:37:09 -0800

    Change target address

--- mqtt.py
+++ mqtt.py
@@ -18,10 +18,9 @@ class MQTT(mqtt.Client):
         print(string)
 
     def conn(self):
-        self.connect("10.9.8.183", 1883, 60)
+        self.connect("10.9.8.184", 1883, 60)
 
     def run(self):
-        #self.connect("m2m.eclipse.org", 1883, 60)
         self.subscribe("$SYS/#", 0)
 
         rc = 0
