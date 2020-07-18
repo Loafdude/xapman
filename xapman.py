23d18391ae2168c5965eeeebf7fb8fd9de4e7033
Dean Rispin <drispin@marinepartssupply.com>
2020-07-17 22:11:40 -0700

    disable matrix scanning

--- xapman.py
+++ xapman.py
@@ -465,18 +465,18 @@ class XapUnit(object):
             self.refreshData()
             self.scanOutputChannels()
             self.scanInputChannels()
-            self.scanMatrix()
+            #self.scanMatrix()
             print("  Scanning Output Channels...")
             for id, channel in self.output_channels.items():
                 channel.initialize()
             print("  Scanning Input Channels...")
             for id, channel in self.input_channels.items():
                 channel.initialize()
-            print("  Scanning Matrix...")
-            for y, row in self.matrix.items():
-                for x, matrix_item in row.items():
-                    if matrix_item:
-                        matrix_item.initialize()
+            # print("  Scanning Matrix...")
+            # for y, row in self.matrix.items():
+            #     for x, matrix_item in row.items():
+            #         if matrix_item:
+            #             matrix_item.initialize()
             print("  Scanning Gating Groups...")
             for group, data in self.gating_groups.items():
                 self.gating_groups[group].initialize()
